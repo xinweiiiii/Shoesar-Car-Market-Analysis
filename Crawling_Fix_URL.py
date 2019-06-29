680903744a33aaa19877ed8c6ffb4fd222eac518
xinweiiiii <43470166+xinweiiiii@users.noreply.github.com>
2019-06-29 21:19:14 +0800

    Cleaning of data

--- Crawling_Fix_URL.py
+++ Crawling_Fix_URL.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 dealercode = 1
 
 def extractdate(url, dealercode):
+
+  keyData = {"Number plate": "", "Brand": "","Model detail": "","Year":"", "Kilometres": "", "Price": "", "Transmission": "", "Dealer Code": "", "Engine size": "" }
 #website 1 
   page = urllib.request.urlopen(url)
-  soup = BeautifulSoup(page)
+  soup = BeautifulSoup(page,features="html.parser")
 
   #store the car header
   title = str(soup.find_all(class_="title-content"))
@@ -23,23 +25,32 @@ def extractdate(url, dealercode):
 
 
   #store all the car information for keydetail section
-  title1 = str(soup.find_all(class_="motors-attribute-value"))
-  allKeyDetails = title1.split("</span>")
-
-  keyDetail1 = []
-  for entry in allKeyDetails:
-      lSymbol = entry.find(">")
-      tempEntry = entry[lSymbol+1:]
-      checkForSymbol = tempEntry.find("<")
-      if (checkForSymbol != -1): #check whether can the symbol be found
-        tempEntry = tempEntry[:checkForSymbol]
-      if (tempEntry != ""): #check whether that particular label is empty or not
-        keyDetail1.append(tempEntry)
+  title1 = str(soup.find_all(class_="attributes-box key-details-box"))
+  allKeyDetails = title1.split("</li>")
+  
+  keyAttribute = ["Number plate", "Kilometres", "Engine size", "Transmission", "Model detail"]
+  
+  for attribute in keyAttribute:
+    for entry in allKeyDetails:
+      if (attribute in entry):
+        extractattribute = entry.split("</span>")
+        listlen = len(extractattribute)
+        keyInfo = extractattribute[listlen-2]
+        if ("font" in keyInfo):
+            tempInfo = keyInfo.split("<font")
+            lSymbol = tempInfo[0].rfind(">")
+            tempEntry = tempInfo[0][lSymbol+1:]
+            keyData[attribute] = tempEntry
+        else: 
+          lSymbol = keyInfo.rfind(">")
+          tempEntry = keyInfo[lSymbol+1:]
+          keyData[attribute] = tempEntry
+   
 
   #store price of vehicle
   title2 = str(soup.find(class_="large-text buynow-details asking-price-text"))
   if (title2 == "None"):
-    price = "Bidding - No Price"
+    price = "Bidding - No Price" #can consider searching for initial bidding price
   else: 
     extractPricePos1 = title2.rfind("</span>")
     extractFull = title2[:extractPricePos1]
@@ -48,24 +59,19 @@ def extractdate(url, dealercode):
   
 
   #consolidating details 
-  model = keyDetail1[10]
+  model = keyData["Model detail"]
   checkduplicate = brandModel.find(model)
   if (checkduplicate == "-1"):
-    keyDetail1.append(brandModel)
+    keyData["Brand"] = brandModel
   else:
     newModel = brandModel[:checkduplicate-1]
-    keyDetail1.append(newModel)
+    keyData["Brand"] = newModel
 
-  keyDetail1.append(year)
-  keyDetail1.append(price)
-  newkeyDetail = []
-  for check in keyDetail1:
-    if (check != "Yes" and check != "No"):
-      newkeyDetail.append(check)
+  keyData["Year"] = year
+  keyData["Price"] = price
+  keyData["Dealer Code"] = dealercode
 
-  requireInformation = [newkeyDetail[0], newkeyDetail[12],newkeyDetail[10], newkeyDetail[13], newkeyDetail[1], newkeyDetail[14], newkeyDetail[6], dealercode, newkeyDetail[5]]
-
-  return requireInformation
+  return keyData
 
 
 
@@ -80,7 +86,10 @@ def insertintoDB(singleData, dealercode):
 
   mycursor = mydb.cursor()
   sql =  " INSERT INTO carinfo (carplatenumber, brand, model, yearofmanufactured, milleage, price, transmission,dealercode,enginecapacity) VALUES (%s, %s, %s,%s,%s,%s,%s,%s,%s)"
-  insert_tuple = (singleData[0], singleData[1], singleData[2], singleData[3], singleData[4], singleData[5], singleData[6], singleData[7], singleData[8])
+  insert_tuple = []
+  for data in singleData:
+    insert_tuple.append(singleData[data])
+
   mycursor.execute(sql, insert_tuple)
   mydb.commit()
 
@@ -88,13 +97,16 @@ def insertintoDB(singleData, dealercode):
   dealercode += 1
 
 
+#Auto run page to generate URL
 urltest = "https://www.trademe.co.nz/motors/used-cars/holden/auction-2203458038.htm?rsqid=77406652aa6b40d58b6e1dae8d644e6e-001"
 
 
 urltest1 = "https://www.trademe.co.nz/motors/used-cars/toyota/auction-2207225515.htm?rsqid=d1a45702bdb144e5b3a24f7efb34ef72-001"
 
 urltest2 = "https://www.trademe.co.nz/motors/used-cars/mercedesbenz/auction-2207776231.htm?rsqid=7b28d9b8e8254592b7031e9ad1330357-001"
-singleData = extractdate(urltest2, dealercode) 
+singleData = extractdate(urltest, dealercode) 
+print(singleData)
+#dealer code incomplete --> Require update (Dealer address)
 
 insertintoDB(singleData, dealercode)
 
